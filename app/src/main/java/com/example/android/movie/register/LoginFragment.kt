a5e95fd461c9a401967755f0672875fd66c54cae
AlexGolovach <alexgolovach1998@gmail.com>
2019-04-26 15:41:55 +0300

    Added TextWatcherAdapter

--- app/src/main/java/com/example/android/movie/register/LoginFragment.kt
+++ app/src/main/java/com/example/android/movie/register/LoginFragment.kt
@@ -1,12 +1,17 @@
 package com.example.android.movie.register
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android.movie.R
+import com.example.android.movie.home.HomeActivity
+import com.example.android.movie.utils.TextWatcherAdapter
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
 
@@ -14,13 +19,51 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_login, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        initToolbar(view)
+
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListeners()
+    }
+
+    private fun initListeners() {
+
+        edit_email.addTextChangedListener(object : TextWatcherAdapter() {
+            override fun afterTextChanged(p0: Editable?) {
+
+                edit_password.addTextChangedListener(object : TextWatcherAdapter() {
+                    override fun afterTextChanged(p0: Editable?) {
+                        updateButtonState()
+                    }
+                })
+            }
+        })
+
         sign_up.setOnClickListener {
             fragmentManager?.beginTransaction()?.replace(R.id.container, SignUpFragment())?.commit()
+        }
+
+        btn_login.setOnClickListener {
+            startActivity(Intent(activity, HomeActivity::class.java))
+
+            activity?.finish()
+        }
+    }
+
+    private fun updateButtonState() {
+        btn_login.isEnabled = true
+    }
+
+    private fun initToolbar(view: View) {
+        view.toolbar.apply {
+            title = getString(R.string.app_name)
         }
     }
 }
--- app/src/main/java/com/example/android/movie/register/SignUpFragment.kt
+++ app/src/main/java/com/example/android/movie/register/SignUpFragment.kt
@@ -6,11 +6,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android.movie.R
+import kotlinx.android.synthetic.main.fragment_sign_up.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
-class SignUpFragment: Fragment(){
+class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_sign_up, container,false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        initToolbar(view)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_sign_up.setOnClickListener {
+            startFragment()
+        }
+    }
+
+    private fun initToolbar(view: View) {
+        view.toolbar.apply {
+            title = getString(R.string.app_name)
+            navigationIcon = resources.getDrawable(R.drawable.ic_arrow_back)
+            setNavigationOnClickListener {
+                startFragment()
+            }
+        }
+    }
+
+    private fun startFragment() {
+        fragmentManager?.beginTransaction()?.replace(R.id.container, LoginFragment())
+            ?.commit()
+    }
 }
