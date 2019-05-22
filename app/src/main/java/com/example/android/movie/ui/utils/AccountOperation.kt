1e0686a25090a8c0e899b9d13eb7e0503766318d
AlexGolovach <alexgolovach1998@gmail.com>
2019-05-22 20:16:40 +0300

    Added classes AccountOperation, ChromeClient, Data and IdGenerator for user

---
+++ app/src/main/java/com/example/android/movie/ui/utils/AccountOperation.kt
@@ -0,0 +1,45 @@
+package com.example.android.movie.ui.utils
+
+import android.content.Context
+import com.example.android.database.App
+import com.example.android.database.model.User
+import com.google.gson.Gson
+
+object AccountOperation {
+
+    private const val APP_PREFERENCES = "ACCOUNT"
+    private const val USER = "USER"
+    private const val SAVE_ACC = "SAVE_ACCOUNT"
+
+    private val sharedPreferences =
+        App.get()?.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+    private val editor = sharedPreferences?.edit()
+
+    private val gson = Gson()
+
+    fun getAccount(): User {
+        val user = sharedPreferences?.getString(USER, "")
+
+        return gson.fromJson(user, User::class.java)
+    }
+
+    fun deleteAccountInformation() {
+        editor?.remove(USER)
+        editor?.remove(SAVE_ACC)
+        editor?.clear()
+        editor?.apply()
+    }
+
+    fun createAccount(user: User, saveAcc: Boolean) {
+        val account = gson.toJson(user)
+
+        editor?.putBoolean(SAVE_ACC, saveAcc)
+        editor?.putString(USER, account)
+        editor?.apply()
+    }
+
+    fun isAccountSaved(): Boolean?{
+
+        return sharedPreferences?.getBoolean(SAVE_ACC,false)
+    }
+}
---
+++ app/src/main/java/com/example/android/movie/ui/utils/Data.kt
@@ -0,0 +1,18 @@
+package com.example.android.movie.ui.utils
+
+import android.os.Bundle
+
+fun getBundleWithId(key: String, id: Int?): Bundle {
+    val bundle = Bundle()
+    id?.let { bundle.putInt(key, it) }
+
+    return bundle
+}
+
+fun convertTime(time: Int?): String? {
+
+    val hours = time?.div(60)
+    val minutes = hours?.times(60)?.let { time.minus(it) }
+
+    return "$hours hr $minutes min"
+}
