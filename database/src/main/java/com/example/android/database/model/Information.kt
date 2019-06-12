d90dabf002a1c214802e1ecdcd274cd9758583c2
AlexGolovach <alexgolovach1998@gmail.com>
2019-06-12 17:03:53 +0300

    Added tables for movie details

---
+++ database/src/main/java/com/example/android/database/model/Information.kt
@@ -0,0 +1,10 @@
+package com.example.android.database.model
+
+data class Information(
+    val id: Int?,
+    val title: String?,
+    val description: String?,
+    val date: String?,
+    val runtime: String?,
+    val image: String?
+)
--- database/src/main/java/com/example/android/database/repository/DBHelperRepository.kt
+++ database/src/main/java/com/example/android/database/repository/DBHelperRepository.kt
@@ -1,6 +1,9 @@
 package com.example.android.database.repository
 
 import com.example.android.database.Callback
+import com.example.android.database.model.ActorSquad
+import com.example.android.database.model.Information
+import com.example.android.database.model.Movie
 import com.example.android.database.model.User
 
 interface DBHelperRepository{
@@ -12,4 +15,20 @@ interface DBHelperRepository{
     fun deleteUser(user: User)
 
     fun findUser(email: String, password: String, callback: Callback<User>)
+
+    fun addInformation(information: Information)
+
+    fun loadInformation(id: Int, callback: Callback<Information>)
+
+    fun addActorSquad(movieId: Int, actorSquad: List<ActorSquad>)
+
+    fun loadActorSquad(movieId: Int, callback: Callback<List<ActorSquad>>)
+
+    fun addRecommendedMovies(movieId: Int, recommendMovie: List<Movie>)
+
+    fun loadRecommendedMovies(movieId: Int, callback: Callback<List<Movie>>)
+
+    fun addVideo(movieId: Int, video: List<String>)
+
+    fun loadVideo(movieId: Int, callback: Callback<List<String>>)
 }
